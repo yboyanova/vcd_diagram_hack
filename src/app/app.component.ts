19890f819fca2617115a7b72e28a8a9db7bfebd7
dpresiana <dpresiana@vmware.com>
2020-12-11 19:10:26 +0200

    Create Default Node, Invisible node (for edges)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -34,13 +34,8 @@ export class AppComponent {
     { key: 14, name: "VDC Group 2", type: "Data Center Group", parent: 13 },
     { key: 15, name: "VDC Group 3", type: "Data Center Group", parent: 13 },
     { key: 16, name: "Data Center 1", type: "Virtual Data Center", parent: 15 },
-    {
-      key: 17,
-      type: "Edge Gateway",
-      parent: 16,
-      count: 2,
-      category: "Edge"
-    },
+      { key: 6, name: 'Edge 1', type: 'Edge Gateway', parent: 4, category: 'Edge', count: 5 },
+      { parent: 6, category: "Invisible" },
     {
       key: 7,
       name: "Edge 2",
@@ -48,6 +43,7 @@ export class AppComponent {
       parent: 4,
       category: "Edge"
     },
+    { parent: 7, category: "Invisible" },
     {
       key: 8,
       name: "Edge 3",
@@ -55,9 +51,10 @@ export class AppComponent {
       parent: 5,
       category: "Edge"
     },
-    { key: 9, name: "VM 1", type: "Virtual Machine", parent: 5 },
-    { key: 10, name: "VM 2", type: "Virtual Machine", parent: 5 },
-    { key: 11, name: "VM 3", type: "Virtual Machine", parent: 5 },
-    { key: 12, name: "vApp", type: "Virtual Application", parent: 5 }
+    { parent: 8, category: "Invisible" },
+    { key: 9, name: "VM 1", type: "Virtual Machine", status: "active", parent: 5 },
+    { key: 10, name: "VM 2", type: "Virtual Machine", status: "inactive", parent: 5 },
+    { key: 11, name: "VM 3", type: "Virtual Machine", status: "active", parent: 5 },
+    { key: 12, name: "vApp", type: "Virtual Application", status: "active", parent: 5 }
   ]);
 }
--- src/diagram/diagram.component.ts
+++ src/diagram/diagram.component.ts
@@ -1,7 +1,22 @@
-import { Component, AfterViewInit, Input } from "@angular/core";
+import { Component, AfterViewInit, Input, Type } from "@angular/core";
 import * as go from "gojs";
 
 const $ = go.GraphObject.make;
+
+enum TYPE {
+  site = "Site",
+  zone = "Available Zone",
+  group = "Data Center Group",
+  vdc = "Virtual Data Center",
+  edge = "Edge Gateway",
+  vapp = "Virtual Application",
+  vm = "Virtual Machine"
+}
+
+enum STATUS {
+  active = "active",
+  inactive = "inactive"
+}
 
 @Component({
   selector: "app-diagram",
@@ -14,13 +29,20 @@ export class DiagramComponent implements AfterViewInit {
   @Input()
   public model: go.Model;
 
+  roundedRectangleParams = {
+    parameter1: 3,  // set the rounded corner
+    spot1: go.Spot.TopLeft, spot2: go.Spot.BottomRight // make content go all the way to inside edges of rounded corners
+  }
+
   ngAfterViewInit(): void {
     this.diagram = $(go.Diagram, "vcd-diagram", {
       layout: $(go.TreeLayout, {
         treeStyle: go.TreeLayout.StyleLastParents,
         angle: 90,
-        alternateAngle: 0,
         alignment: go.TreeLayout.AlignmentStart,
+        // properties for the "last parents":
+        alternateAngle: 0,
+
         alternateAlignment: go.TreeLayout.AlignmentStart
       }),
       // Enabling collapse or expand
@@ -34,24 +56,73 @@ export class DiagramComponent implements AfterViewInit {
       allowTextEdit: false
     });
     this.diagram.nodeTemplate = $(
-      go.Node,
-      "Auto",
-      { isTreeExpanded: false },
-      { locationSpot: go.Spot.Center },
-      { alignment: new go.Spot(0, 0) },
-      $(go.Shape, "Rectangle", {
-        fill: "white",
+      go.Node, "Auto",
+      {
+        isTreeExpanded: false,
+        locationSpot: go.Spot.Top,
+        alignment: new go.Spot(0, 0),
+        isShadowed: true,
+        shadowBlur: 1,
+        shadowOffset: new go.Point(0, 2),
+        shadowColor: "#D7D7D7",
+        margin: new go.Margin(16, 16, 16, 18)
+      },
+      $(go.Shape, 'RoundedRectangle', this.roundedRectangleParams, {
+        fill: "#ffffff",
         stroke: "lightgray",
-        desiredSize: new go.Size(120, 50)
+        desiredSize: new go.Size(325, 80)
       }),
-      $(
-        go.Panel,
-        "Auto",
-        // $(go.Shape, 'Rectangle', { column: 0, desiredSize: new go.Size(50, 50), fill: '#b0d6eb', margin: 20 }),
-        $(go.TextBlock, { row: 0, column: 1 }, new go.Binding("text", "type")),
-        $(go.TextBlock, { row: 1, column: 1 }, new go.Binding("text", "name"))
-      ),
-      $("TreeExpanderButton")
+      $(go.Panel, "Vertical",
+        $(go.Panel, "Horizontal",
+          $(go.Panel, "Spot",
+            $(go.Shape, "RoundedRectangle", this.roundedRectangleParams, {
+              margin: new go.Margin(0, 16, 0, 0),
+              desiredSize: new go.Size(48, 48),
+              fill: "#89CBDF",
+              stroke: null,
+              alignment: go.Spot.Left
+            },
+              new go.Binding("fill", "type", type => this.getColorByType(type))
+            ),
+            $(go.Picture, {
+              desiredSize: new go.Size(30, 30),
+              alignment: go.Spot.Center,
+              // alignmentFocus: new go.Spot(0, 0, 12, 5),
+            },
+              new go.Binding("source", "", (data) => this.getIconByType(data.type, data.status))
+            )
+          ),
+          $(go.Panel, "Table",
+            $(go.TextBlock,
+              {
+                column: 0,
+                row: 0,
+                alignment: go.Spot.Left,
+                font: "11px Metropolis"
+              },
+              new go.Binding("text", "type")
+            ),
+            $(go.TextBlock,
+              {
+                column: 0,
+                row: 1,
+                alignment: go.Spot.Left,
+                font: "18px Metropolis"
+              },
+              new go.Binding("text", "name")
+            ),
+          ),
+          $(go.Picture, {
+            source: "assets/ellipsis-vertical-line.svg",
+            desiredSize: new go.Size(20, 20),
+            alignment: go.Spot.Right
+          })
+        ),
+        $(go.Panel,
+          { height: 17 },
+          $("TreeExpanderButton")
+        )
+      )
     );
 
     // Edge nodes template
@@ -100,6 +171,16 @@ export class DiagramComponent implements AfterViewInit {
       )
     );
 
+    // Invisible nodes - Hack for Edge link
+    this.diagram.nodeTemplateMap.add(
+      "Invisible",
+      $(
+        go.Node,
+        "Spot",
+        // "visible: false" hides the link
+        { opacity: 0 }
+      ));
+
     // define the Link template
     this.diagram.linkTemplate = $(
       go.Link,
@@ -119,4 +200,37 @@ export class DiagramComponent implements AfterViewInit {
 
     this.diagram.model = this.model;
   }
+
+  getColorByType(type: TYPE): any {
+    switch (type) {
+      case TYPE.site: return "#003D79";
+      case TYPE.zone: return "#0065AB";
+      case TYPE.group: return "#0095D3";
+      case TYPE.vdc: return "#89CBDF";
+      case TYPE.vm: return "#FAFAFA";
+      case TYPE.vapp: return "#565656";
+      default: return "#0095D3";
+    }
+  }
+
+  getIconByType(type: TYPE, status: STATUS = STATUS.active) {
+    switch (type) {
+      case TYPE.site: return "assets/map-marker-solid.svg";
+      case TYPE.zone: return "assets/host-solid.svg";
+      case TYPE.group: return "assets/cloud-scale-line.svg";
+      case TYPE.vdc: return "assets/cloud-solid.svg";
+      case TYPE.vm: {
+        return status === STATUS.active ?
+          "assets/vm-outline-badged-active.svg" :
+          "assets/vm-outline-badged-inactive.svg";
+      };
+      case TYPE.vapp: {
+        return status === STATUS.active ?
+          "assets/vmw-app-outline-badged-active.svg" :
+          "assets/vmw-app-outline-badged-inactive.svg";
+      };
+      default: return "assets/cloud-scale-line.svg";
+    }
+  }
+
 }
